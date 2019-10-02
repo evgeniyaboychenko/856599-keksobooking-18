aee3b71b0f9e9a929953b05e90a90c3abbf0caa6
evgeniyaboychenko <evgeniyaboychenko@gmail.com>
2019-10-02 16:34:24 +0700

    Отображение карточек, валидация формы

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ var WIDTH_MAP = MAP_PINS.clientWidth;
 var TEMPLATE_MAP_CARD = document.querySelector('#card').content.querySelector('.map__card');
 
 var ENTER_KEYCODE = 13;
+var ESC_KEYCODE = 27;
 var windowMap = document.querySelector('.map');
 
 var formAd = document.querySelector('.ad-form');
@@ -101,11 +102,13 @@ var createAd = function (ad) {
   return mapPin;
 };
 
-var ads = createAdsData();
+var adsData = createAdsData();
+
+// добавление объявлений в фрагмент
 var addAds = function () {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < NUMBER_OFFER; i++) {
-    fragment.appendChild(createAd(ads[i]));
+    fragment.appendChild(createAd(adsData[i]));
   }
   return fragment;
 };
@@ -114,6 +117,7 @@ var getPropertyTypeName = function (key) {
   return TYPE_HOUSES_MAP[key];
 };
 
+// создание карточки объявления от объекта метка
 var createCardAd = function (ad) {
   var mapCard = TEMPLATE_MAP_CARD.cloneNode(true);
   var mapCardAvatar = mapCard.querySelector('.popup__avatar');
@@ -167,20 +171,12 @@ var getEndingWordRoom = function (number) {
   }
   return ending;
 };
-
-var addCards = function () {
+// добавление карточки метки в фрагмент
+var addCardMap = function (number) {
   var fragment = document.createDocumentFragment();
-  fragment.appendChild(createCardAd(ads[0]));
+  fragment.appendChild(createCardAd(adsData[number]));
   return fragment;
 };
-
-
-/* временно для 4 дз
-// windowMap.classList.remove('map--faded');
-// добавление обявлений и карточки объявления
-// MAP_PINS.appendChild(addAds());
-// var MAP_FILTERS_CONTAINER = document.querySelector('.map__filters-container');
-// MAP_FILTERS_CONTAINER.before(addCards()); */
 
 // добавление атрибута 'disabled'
 var setDisabledAttribute = function (field) {
@@ -224,8 +220,63 @@ var setAddressOnMapMainMove = function () {
   addressField.setAttribute('value', xMapMain + ', ' + yMapMain);
 };
 
+// -----------------------валидация формы-------------------------
+// валидация Заголовок объявления
+var checkedTitleFields = function () {
+  var titleField = formAd.querySelector('input[name="title"]');
+  titleField.addEventListener('invalid', function () {
+    if (titleField.validity.tooShort) {
+      titleField.setCustomValidity('Заголовок должен быть больше 30 символов');
+    } else if (titleField.validity.valueMissing) {
+      titleField.setCustomValidity('Заголовок должен быть заполнен');
+    } else {
+      titleField.setCustomValidity('');
+    }
+  });
+};
+checkedTitleFields();
+
+// стоимость по типу жилья
+var MIN_PRICE_HOUSES_MAP = {palace: 10000, flat: 1000, house: 5000, bungalo: 0};
+var typeHouseField = formAd.querySelector('select[name="type"]');
+var priceField = formAd.querySelector('input[name="price"]');
+
+priceField.addEventListener('invalid', function () {
+  if (priceField.validity.valueMissing) {
+    priceField.setCustomValidity('Введите цену');
+  } else if (priceField.validity.stepMismatch) {
+    priceField.setCustomValidity('Цена не соответствует шагу в 10');
+  } else if (priceField.validity.rangeOverflow) {
+    priceField.setCustomValidity('Стоимость не может быть больше 1000000');
+  } else {
+    priceField.setCustomValidity('');
+  }
+});
+
+var determinateMinPriceHouse = function () {
+  priceField.setAttribute('placeholder', MIN_PRICE_HOUSES_MAP[typeHouseField.value]);
+  priceField.setAttribute('min', MIN_PRICE_HOUSES_MAP[typeHouseField.value]);
+};
+
+var checkedPriceFields = function () {
+  if (priceField.value < MIN_PRICE_HOUSES_MAP[typeHouseField.value]) {
+    priceField.setCustomValidity('Минимальная стоимость для выбранного типа жилья должна быть выше ' + MIN_PRICE_HOUSES_MAP[typeHouseField.value]);
+  } else {
+    priceField.setCustomValidity('');
+  }
+};
+
+var setFieldsPriceHouse = function () {
+  determinateMinPriceHouse();
+  checkedPriceFields();
+};
+
+determinateMinPriceHouse();
+typeHouseField.addEventListener('change', setFieldsPriceHouse);
+priceField.addEventListener('change', setFieldsPriceHouse);
+
 // проверка соответствия комнат кол-ву гостей
-var checkedFields = function () {
+var checkedGuestsFields = function () {
   if (roomField.value === '1' && guestField.value !== '1') {
     guestField.setCustomValidity('Только 1 гость может быть для 1 комнаты ');
   } else if ((roomField.value === '2' && guestField.value === '3') || (roomField.value === '2' && guestField.value === '0')) {
@@ -239,28 +290,93 @@ var checkedFields = function () {
   }
 };
 
+checkedGuestsFields();
+roomField.addEventListener('change', checkedGuestsFields);
+guestField.addEventListener('change', checkedGuestsFields);
 
-checkedFields();
-roomField.addEventListener('change', function () {
-  checkedFields();
+// ------------------------------------------------------
+// соответствие время выезда времени въезда
+var timeInField = formAd.querySelector('select[name="timein"]');
+var timeOutField = formAd.querySelector('select[name="timeout"]');
+
+timeInField.addEventListener('change', function () {
+  timeOutField.value = timeInField.value;
 });
-guestField.addEventListener('change', function () {
-  checkedFields();
+
+timeOutField.addEventListener('change', function () {
+  timeInField.value = timeOutField.value;
 });
+// ----------------------------------------------------------------------------
 
 setDisabledForm();
 setAddressMapMain();
 
+// ----------------------------------------------------------------------------
 
-var onActiveFormPressEnter = function (evt) {
-  if (evt.keyCode === ENTER_KEYCODE) {
-    setAddressOnMapMainMove();
-    removeDisabledForm();
-    removeListenerActMapMain(evt);
+var MAP_FILTERS_CONTAINER = document.querySelector('.map__filters-container');
+var flagShowCard = false;
+
+// при нажатии на ESC удалить карточку
+var onClosePopupPressEscKey = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    onClosePopupPressClick();
   }
 };
 
+// при закрытии формы удалить карточку и удалить обработчик при Esc
+var onClosePopupPressClick = function () {
+  removeCard();
+  document.removeEventListener('keydown', onClosePopupPressEscKey);
+};
+
+// удалить карточку
+var removeCard = function () {
+  var popupCard = windowMap.querySelector('.popup');
+  popupCard.remove();
+  flagShowCard = false;
+};
+
+// создать карточку объявления при нажатии на метку
+var onAddCardClickPin = function (evt) {
+  var pinClickMap = evt.target.closest('button[type="button"]');
+  if (!pinClickMap) {
+    return;
+  }
+  if (flagShowCard) {
+    removeCard();
+  }
+  var pins = MAP_PINS.children;
+  for (var i = 0; i < pins.length; i++) {
+    if (pins[i] === pinClickMap) {
+      MAP_FILTERS_CONTAINER.before(addCardMap(i - 2));
+      flagShowCard = true;
+    }
+  }
+  var popupCard = windowMap.querySelector('.popup');
+  var buttonClosePopup = popupCard.querySelector('.popup__close');
+  buttonClosePopup.addEventListener('click', onClosePopupPressClick);
+  document.addEventListener('keydown', onClosePopupPressEscKey);
+};
+
+// создать карточку при нажатии на enter
+var onAddCardPressEnterPin = function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    onAddCardClickPin(evt);
+  }
+};
+
+// активация формы при нажатии Enter на главную метку
+var onActiveFormPressEnter = function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    onActiveForm(evt);
+  }
+};
+
+// активация формы
 var onActiveForm = function (evt) {
+  windowMap.classList.remove('map--faded');
+  formAd.classList.remove('ad-form--disabled');
+  MAP_PINS.appendChild(addAds());
   setAddressOnMapMainMove();
   removeDisabledForm();
   removeListenerActMapMain(evt);
@@ -275,7 +391,16 @@ var removeListenerActMapMain = function () {
 var addListenerActMapMain = function () {
   mapPinMain.addEventListener('mousedown', onActiveForm);
   mapPinMain.addEventListener('keydown', onActiveFormPressEnter);
+
+};
+
+// добавить обраточкики события для показа карточки объявления при нажатии на метку
+var addListenerAddCardPressPin = function () {
+  MAP_PINS.addEventListener('mousedown', onAddCardClickPin);
+  MAP_PINS.addEventListener('keydown', onAddCardPressEnterPin);
 };
 
 addListenerActMapMain();
+addListenerAddCardPressPin();
+
 
